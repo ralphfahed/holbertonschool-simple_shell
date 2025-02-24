6bc9134fe8e1e6e99160a7e99de9debe130c5db0
ralph <ralphh.fahed@gmail.com>
2025-02-24 13:09:36 +0000

    task

--- shell.c
+++ shell.c
@@ -1,66 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
-    char *buf = NULL;
-    size_t count = 0;
-    ssize_t nread;
-    char *array[2];
-    pid_t child_pid;
-    int status;
+char *buf= NULL;
+size_t buf_size = 0;
+char *token;
+int status, i = 0;
+char **array;
+pid_t child_pid;
 
-    while (1) {
-        write(STDOUT_FILENO, "MyShell$ ", 9);
 
-        nread = getline(&buf, &count, stdin);
+	while(1){
+		write(1, "#cisfun$ ", 9);
+		getline(&buf, &buf_size, stdin);
+		token = strtok(buf, "\t\n");
+		array = malloc(sizeof(char *) * 1024);
+	
+		while(token)
+		{
+			array[i] = token;
+			token = strtok(NULL, "\t\n");
+			i++;
+		
+		}
+		array[i] = NULL;
+		child_pid = fork();
 
-        if (nread == -1) {
-            free(buf);
-            return (0);
-        }
+		if (child_pid == 0)
+		{
+			if(execve(array[0], array ,  NULL) == -1 )
+				perror("Error");
 
-        if (buf[nread - 1] == '\n') {
-            buf[nread - 1] = '\0';
-        }
-
-        array[0] = strtok(buf, " \n");
-        array[1] = NULL;
-
-        if (array[0] == NULL) {
-            continue;
-        }
-
-        if (strcmp(array[0], "exit") == 0) {
-            free(buf);
-            exit(0);
-        }
-
-        if (strcmp(array[0], "ls") == 0) {
-            array[0] = "/bin/ls";
-        }
-
-        child_pid = fork();
-        if (child_pid == -1) {
-            perror("Failed to create process");
-            exit(41);
-        }
-
-        if (child_pid == 0) {
-            if (execve(array[0], array, NULL) == -1) {
-                perror("Command not found");
-                exit(1);
-            }
-        } else {
-            wait(&status);
-        }
-    }
-
-    free(buf); 
-    return (0);
+		}
+		else
+		{
+			wait(&status);
+		}
+		i= 0;
+	free(array);
 }
-
+}
