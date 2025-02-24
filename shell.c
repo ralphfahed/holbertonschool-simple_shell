252b2c89c25b6db8373b998bddfb4ebdd44485f4
ralph <ralphh.fahed@gmail.com>
2025-02-24 08:46:22 +0000

    task 2

--- shell.c
+++ shell.c
@@ -1,7 +1,7 @@
-#include "shell.h"
+#include "main.h"
 
 /**
-  * main - Getline function
+  * main - Simple shell implementation using getline
   * @argc: Argument count
   * @argv: Array of argument values
   *
@@ -10,61 +10,73 @@
 
 int main(int argc, char **argv)
 {
-        (void)argc, (void)argv;
-        char *buf = NULL, *token;
-        size_t count = 0;
-        ssize_t nread;
-        pid_t child_pid;
-        int i, status;
-        char **array;
+    char *buf = NULL, *token;
+    size_t count = 0;
+    ssize_t nread;
+    pid_t child_pid;
+    int i, status;
+    char **array;
 
-        while (1)
+    (void)argc, (void)argv; /* Suppress unused parameter warnings */
+
+    while (1)
+    {
+        write(STDOUT_FILENO, "MyShell$ ", 9);
+
+        nread = getline(&buf, &count, stdin);
+        if (nread == -1)
         {
-                write(STDOUT_FILENO, "MyShell$ ", 9);
-
-                nread = getline(&buf, &count, stdin);
-
-                if (nread ==  -1)
-                {
-                        perror("Exiting shell");
-                        exit(1);
-                }
-
-                token = strtok(buf, " \n");
-
-                array = malloc(sizeof(char*) * 1024);
-                i = 0;
-
-                while (token)
-                {
-                        array[i] = token;
-                        token = strtok(NULL, " \n");
-                        i++;
-                }
-
-                array[i] = NULL;
-
-                child_pid = fork();
-
-                if (child_pid == -1)
-                {
-                        perror("Failed to create.");
-                        exit (41);
-                }
-
-                if (child_pid == 0)
-                {
-                        if (execve(array[0], array, NULL) == -1)
-                        {
-                                perror("Failed to execute");
-                                exit(97);
-                        }
-                }
-                else
-                {
-                        wait(&status);
-                }
+            perror("Exiting shell");
+            free(buf);
+            exit(1);
         }
-        free(buf);
-        return (0);
+
+        array = malloc(sizeof(char *) * 1024);
+        if (!array)
+        {
+            perror("Memory allocation failed");
+            free(buf);
+            exit(1);
+        }
+
+        token = strtok(buf, " \n");
+        i = 0;
+        while (token)
+        {
+            array[i] = token;
+            token = strtok(NULL, " \n");
+            i++;
+        }
+        array[i] = NULL;
+
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            perror("Failed to create process");
+            free(array);
+            free(buf);
+            exit(41);
+        }
+
+        if (child_pid == 0)
+        {
+            if (execve(array[0], array, NULL) == -1)
+            {
+                perror("Failed to execute");
+                free(array);
+                free(buf);
+                exit(97);
+            }
+        }
+        else
+        {
+            wait(&status);
+        }
+
+        free(array);
+    }
+    
+    free(buf);
+    return (0);
 }
+
