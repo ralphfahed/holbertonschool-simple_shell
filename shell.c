85a616aa5f345923c05478cab9c041ff6aa314ea
ralph <ralphh.fahed@gmail.com>
2025-02-24 10:04:33 +0000

    task

--- shell.c
+++ shell.c
@@ -2,57 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
 
-#define BUFFER_SIZE 1024
-
-extern char **environ;  /* Declare environ */
-
-/* Function to display the prompt */
 void display_prompt() {
-    write(1, "#cisfun$ ", 9); /* Write the prompt to stdout */
+    /* Display the prompt before reading input */
+    write(STDOUT_FILENO, "#cisfun$ ", 9);
 }
 
-/* Function to read a line of input */
 char *read_input() {
-    char *buffer = malloc(BUFFER_SIZE);
-    if (!buffer) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
+    char *command = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    /* Use getline instead of fgets */
+    nread = getline(&command, &len, stdin);
+    if (nread == -1) {
+        /* Handle EOF or error */
+        free(command);
+        return NULL;
     }
-    if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
-        free(buffer);
-        return NULL; /* EOF detected */
+    
+    /* Remove the newline character from input */
+    if (nread > 0 && command[nread - 1] == '\n') {
+        command[nread - 1] = '\0';
     }
-    return buffer;
+
+    return command;
 }
 
-/* Function to execute a command */
 void execute_command(char *command) {
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0) {  /* Child process */
-        char *argv[2];  /* Declare argv here */
-        argv[0] = command;  /* Set the command */
-        argv[1] = NULL;  /* Null terminate the argument list */
-        if (execve(command, argv, environ) == -1) {  /* Try to execute the command */
-            perror("./shell");  /* Print error message if execve fails */
-            exit(EXIT_FAILURE);
-        }
-    } else {  /* Parent process */
-        wait(NULL);  /* Wait for the child process to finish */
+    char *argv[] = {command, NULL};  /* Array of arguments for execve */
+    if (execve(command, argv, NULL) == -1) {  /* Try to execute the command */
+        perror("execve");
     }
 }
 
 int main() {
     char *command;
-    size_t len;
 
     while (1) {
         display_prompt();  /* Display prompt before reading the command */
@@ -63,14 +48,8 @@ int main() {
             break;
         }
 
-        len = strlen(command);
-        if (len > 0 && command[len - 1] == '\n') {
-            command[len - 1] = '\0';  /* Remove the newline character */
-        }
-
-        /* If the command is not empty, attempt to execute it */
         if (strlen(command) > 0) {
-            execute_command(command);  /* Execute the command without printing prompt again */
+            execute_command(command);  /* Execute the command */
         }
 
         free(command);  /* Free the allocated memory for the command */
