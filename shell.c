6a81e58dd624cc6a70f09420fa2135a32867009d
ralph <ralphh.fahed@gmail.com>
2025-02-25 20:02:08 +0000

    task

--- shell.c
+++ shell.c
@@ -13,22 +13,27 @@ extern char **environ;  /* The global environment variable */
 
 /* Function to search for the command in the PATH directories */
 char *find_command_in_path(const char *command) {
-    char *path_env, *path_copy, *path, *full_path;
+    char *path_env;
+    char *path_copy;
+    char *path;
+    char *full_path;
     
+    if (access(command, X_OK) == 0) {
+        return strdup(command);
+    }
+
     path_env = getenv("PATH");
     if (!path_env || strlen(path_env) == 0) {
         return NULL;
     }
     
-    path_copy = strdup(path_env); /* Create a copy of PATH to avoid modifying the original */
+    path_copy = strdup(path_env);
     if (!path_copy) {
         perror("strdup");
         exit(EXIT_FAILURE);
     }
     
     path = strtok(path_copy, ":");
-    full_path = NULL;
-    
     while (path != NULL) {
         full_path = malloc(strlen(path) + strlen(command) + 2);
         if (!full_path) {
@@ -51,23 +56,29 @@ char *find_command_in_path(const char *command) {
 }
 
 void execute_command(char *command) {
-    char *args[2];
+    char *args[MAX_COMMAND_LENGTH / 2];
     char *command_path;
-    
-    if (command[0] == '/' || command[0] == '.') {
-        command_path = command;
-    } else {
-        command_path = find_command_in_path(command);
+    int i = 0;
+    char *token;
+
+    token = strtok(command, " ");
+    while (token) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
     }
-    
+    args[i] = NULL;
+
+    if (args[0][0] == '/' || args[0][0] == '.') {
+        command_path = args[0];
+    } else {
+        command_path = find_command_in_path(args[0]);
+    }
+
     if (command_path == NULL) {
         write(STDERR_FILENO, "Command not found\n", 18);
         exit(127);
     }
-    
-    args[0] = command_path;
-    args[1] = NULL;
-    
+
     if (execve(command_path, args, environ) == -1) {
         perror("execve");
         exit(EXIT_FAILURE);
@@ -76,7 +87,6 @@ void execute_command(char *command) {
 
 void read_command(char *buffer) {
     int n;
-    
     n = read(STDIN_FILENO, buffer, MAX_COMMAND_LENGTH - 1);
     if (n == -1) {
         perror("read");
@@ -84,7 +94,7 @@ void read_command(char *buffer) {
     } else if (n == 0) {
         exit(EXIT_SUCCESS);
     }
-    
+
     buffer[n] = '\0';
     if (buffer[n - 1] == '\n') {
         buffer[n - 1] = '\0';
@@ -103,34 +113,29 @@ void print_env(void) {
 int main(void) {
     char command[MAX_COMMAND_LENGTH];
     pid_t pid;
-    
+
     while (1) {
         if (isatty(STDIN_FILENO)) {
             write(STDOUT_FILENO, "#cisfun$ ", 9);
         }
-        
+
         read_command(command);
-        
+
         if (strcmp(command, "exit") == 0) {
             exit(EXIT_SUCCESS);
         }
-        
+
         if (strcmp(command, "env") == 0) {
             print_env();
             continue;
         }
-        
-        if (command[0] != '/' && command[0] != '.' && find_command_in_path(command) == NULL) {
-            write(STDERR_FILENO, "Command not found\n", 18);
-            continue;
-        }
-        
+
         pid = fork();
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        
+
         if (pid == 0) {
             execute_command(command);
         } else {
@@ -139,4 +144,3 @@ int main(void) {
     }
     return 0;
 }
-
